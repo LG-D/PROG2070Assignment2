4be5c02fe1f22fcfa3caa5da44cbb194b853e9ee
Lirjeta Gashi <lirjeta.gashi1@gmail.com>
2020-02-24 00:48:03 -0500

    Completed TriangleSolver class

---
+++ PROG2070Assignment2/TriangleSolver.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROG2070Assignment2
+{
+    public static class TriangleSolver
+    {
+        public static string Analyze(int a, int b, int c)
+        {
+            bool formsTriangle = false;
+
+            StringBuilder output = new StringBuilder();
+
+            if ((a + b > c) && (a + c > b) && (b + c > a))
+            {
+                formsTriangle = true;
+                output.AppendLine("The triangle can be formed");
+            }
+            else
+            {
+                formsTriangle = false;
+                output.AppendLine("The triangle cannot be formed ");
+            }
+
+            if (formsTriangle)
+            {
+                if (a == b && b == c)
+                {
+                    output.AppendLine("The triangle is Equilateral");
+                }
+                else if (a == b || a == c || b == c)
+                {
+                    output.AppendLine("The triangle is Isosceles");
+                }
+                else
+                {
+                    output.AppendLine("The triangle is Scalene");
+                }
+            }
+            return output.ToString();
+        }
+    }
+}
