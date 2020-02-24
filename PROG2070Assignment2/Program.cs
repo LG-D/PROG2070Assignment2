03368efe64e6481d472bf68e85587cb11f8a2efd
Lirjeta Gashi <lirjeta.gashi1@gmail.com>
2020-02-24 00:51:49 -0500

    Contains completed console application

--- PROG2070Assignment2/Program.cs
+++ PROG2070Assignment2/Program.cs
@@ -10,6 +10,60 @@ namespace PROG2070Assignment2
     {
         static void Main(string[] args)
         {
+            string userInput;
+            string userSide1;
+            string userSide2;
+            string userSide3;
+
+            bool validInput = true;
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("1. Enter triangle dimensions");
+                Console.WriteLine("2. Exit");
+                Console.WriteLine();
+                Console.Write("Your Answer: ");
+                userInput = Console.ReadLine();
+                try
+                {
+                    int.TryParse(userInput, out int input);
+                    if (input == 1)
+                    {
+                        validInput = true;
+                        Console.Write("Enter 1st number: ");
+                        userSide1 = Console.ReadLine();
+                        Console.Write("Enter 2nd number: ");
+                        userSide2 = Console.ReadLine();
+                        Console.Write("Enter 3rd number: ");
+                        userSide3 = Console.ReadLine();
+                        Console.WriteLine();
+
+                        int.TryParse(userSide1, out int side1);
+                        int.TryParse(userSide2, out int side2);
+                        int.TryParse(userSide3, out int side3);
+
+                        Console.WriteLine(TriangleSolver.Analyze(side1, side2, side3));
+                    }
+                    else if (input == 2)
+                    {
+                        validInput = true;
+                        Environment.Exit(0);
+                    }
+                    else
+                    {
+                        validInput = false;
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Incorrect input, please try again");
+                    validInput = false;
+                }
+            } while (!validInput);
+
+            Console.ReadLine();
         }
+    
     }
 }
